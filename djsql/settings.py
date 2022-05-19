c67159db6a379de3d8fff3c3308e8ddab4efd4eb
Prasanth <prasanth.mvk@gmail.com>
2022-05-19 14:11:52 +0100

    Update settings.py

--- djsql/settings.py
+++ djsql/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!dn1$duxbz)z@u%6)*wf#94638crf9%q&6c!wadlr(*r_535el'
+SECRET_KEY = 'django-insecure-<<secret-key>>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
